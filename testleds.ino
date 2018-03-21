48f99e7222218650eed78bc19d8b02ab8dd33149
Ian House <pattheaux@yahoo.com>
2018-03-20 20:55:01 -0400

    more patterns

--- testleds.ino
+++ testleds.ino
@@ -32,6 +32,8 @@ int cycle[] = { 5000, 5000, 5000, 5000 };
 
 CRGBArray<TOT_LEDS> leds;
 CRGB patt[NUM_PATT];
+CRGB patt2[NUM_PATT];
+
 //CRGBArray<NUM_PATT> patt;
 //unsigned long patt[NUM_PATT];
 
@@ -52,6 +54,7 @@ void mm(mode_t *m,int mode);
 void mrand(mode_t *m,int mode);
 void mdrip(mode_t *m,int mode);
 void mdraw(mode_t *m,int mode);
+void mslide(mode_t *m,int mode);
 
 CRGB white[] = { 0xffffff };
 CRGB purple[] = { 0xFF00FF };
@@ -62,12 +65,17 @@ CRGB black[] = { 0x000000 };
 CRGB greens[] = { 0x00E00, 0x004000, 0x000400, 0x000200, 0x001000 };
 
 mode_t modes[] = {
+  { 64, 1, gnum_patt, patt2, 0, mslide },
+
   { 64, 1, gnum_patt, patt, 3, mdraw },
   { 64, 1, gnum_patt, patt, 0, mdraw },
   { 64, 1, gnum_patt, patt, 1, mdraw },
+  
   { 64, 1, 5, greens, 1, mdrip },
   { 64, 1, gnum_patt, patt, 1, mdrip },
   { 64, 1, gnum_patt, patt, 0, mdrip },
+
+  { 64, 1, gnum_patt, patt2, 4, mm },
   
   { 64, 1, gnum_patt, patt, 0, mm },
   { 16, 4, gnum_patt, patt, 0, mm },
@@ -78,6 +86,8 @@ mode_t modes[] = {
   { 64, 1, gnum_patt, patt, 0, mrand },
   { 64, 1, gnum_patt, patt, 1, mrand },
   { 16, 4, gnum_patt, patt, 0, mrand },
+
+  { 0,0,0,patt,0,mm },
 
   //{ 1, 64, gnum_patt, patt, 0, mm },
   { 1, 64, 1, white, 1, mm },
@@ -90,13 +100,18 @@ int nmodes = 14;
 
 CRGB grid[16][4];
 
-
 void cleargrid() {
   for(int i=0;i<16;i++) {
     for(int j=0;j<4;j++) {
       grid[i][j] = black[0];
     }
   }
+}
+
+CRGB scolor(unsigned char val,int h) {
+  CRGB ret = CHSV(h,255,val);
+
+  return ret;
 }
 
 int v[256];
@@ -115,6 +130,53 @@ void setup() {
   FastLED.addLeds<WS2812B,4>(leds, TOT_LEDS);
   int type = 3;
 
+  for(int i=0;i<30;i++) {
+    if(modes[i].num_leds == 0) {
+      nmodes = i;
+      break;
+    }
+  }
+
+  for(int i=0;i<NUM_PATT;i++) {
+    float f = (float)(i+1) / NUM_PATT;
+    int v = 200;//(millis()/200) % 255;
+    int h = (int)(f*255.0);
+    //h = 127;
+    if((h < 0) || (h > 255)) h = 127;
+    patt[i] = CHSV(h,255,v);
+  }
+
+  {
+    int b = 0x000040;
+    int g = 0x000000;
+    int i = 0;
+    
+    int h = 100;
+    for(i=0;i<10;i++) {
+      patt2[i] = scolor(i*22,h);
+    }
+    int p = i-1;
+    for(;i<20;i++,p--) {
+      patt2[i] = patt2[p];
+    }
+    for(;i<NUM_PATT;i++) {
+      patt2[i] = 0x000002;
+    }
+    /*
+    patt2[0] = scolor(0x08);
+    patt2[1] = scolor(0x0F);
+    patt2[2] = scolor(0x10);
+    patt2[3] = scolor(0xE0);
+    patt2[4] = patt2[2];
+    patt2[5] = patt2[1];
+    patt2[6] = patt2[0];
+    for(int i=7;i<NUM_PATT;i++) {
+      patt2[i] = 0x000003;
+    }
+    */
+  }
+
+  /*
   if(type == 0) {
     for(int i=0;i<NUM_PATT;i++) {
       patt[i] = 0xf0f00f;
@@ -166,10 +228,10 @@ void setup() {
       patt[i] = 0x00ff00;
     }
   }
-
+  */
 }
 
-int pcount = 0;
+//int pcount = 0;
 
 unsigned long slowpos(unsigned long p) {
   return p/4;
@@ -187,6 +249,133 @@ void drawgrid(mode_t *m) {
       leds[ii+off] = grid[i][row];
     }
   }
+}
+
+int rot(int ii) {
+  switch(ii) {
+    case 0: return 0;
+    case 1: return 7;
+    case 2: return 8;
+    case 3: return 15;
+
+    case 4: return 1;
+    case 5: return 6;
+    case 6: return 9;
+    case 7: return 14;
+
+    case 8: return 2;
+    case 9: return 5;
+    case 10: return 10;
+    case 11: return 13;
+
+    case 12: return 3;
+    case 13: return 4;
+    case 14: return 11;
+    case 15: return 12;
+  }
+  return 0;
+}
+
+void mslide(mode_t *m,int mode) {
+  static int h = 100;
+  static int v = 255;
+  static int dir = -1;
+  static CRGB color[16];
+  
+  unsigned int pos = gpos;
+  if(pos % 3 != 0) return;
+  pos /= 3;
+
+  if((pos % 8 == 0) || (dir < 0)) {
+    int cpat = random(0,6);
+    
+    h = random(0,256);
+    v = random(20,256);
+
+    for(int i=0;i<16;i++) {
+      if(cpat == 0) color[i] = CHSV(h,random(0,256),v);
+      else if(cpat == 1) color[i] = black[0];
+      else if((cpat == 4) && (i%2==0)) color[i] = black[0];
+      else if((cpat == 3) && (i%2==0)) color[i] = red[0];
+      else if(cpat == 3) color[i] = white[0];
+      else color[i] = CHSV(h,255,v);
+    }
+    if(cpat == 1) color[6] = color[5] = color[9] = color[10] = CHSV(h,255,v);
+    if(cpat == 2) color[6] = color[5] = color[9] = color[10] = black[0];
+    
+    dir++;
+    dir %= 6;
+
+    dir = random(0,6);
+    //dir = 4;
+  }
+
+  int p=0;
+  if(dir == 0) {
+    for(int i=15;i>3;i--) {
+      for(int j=0;j<4;j++) {
+        grid[i][j] = grid[i-4][j];
+      }
+    }
+    for(int i=0;i<4;i++) {
+      for(int j=0;j<4;j++) {
+        grid[i][j] = color[p++];
+      }
+    }
+  } else if(dir == 1) {
+    for(int i=0;i<12;i++) {
+      for(int j=0;j<4;j++) {
+        grid[i][j] = grid[i+4][j];
+      }
+    }
+    for(int i=12;i<16;i++) {
+      for(int j=0;j<4;j++) {
+        grid[i][j] = color[p++];
+      }
+    }
+  } else if(dir == 2) {
+    for(int j=3;j>0;j--) {
+      for(int i=0;i<16;i++) {
+        grid[i][j] = grid[i][j-1];
+      }
+    }
+    for(int i=0;i<16;i++) {
+      grid[i][0] = color[p++];
+    }
+  } else if(dir == 3) {
+    for(int j=0;j<3;j++) {
+      for(int i=0;i<16;i++) {
+        grid[i][j] = grid[i][j+1];
+      }
+    }
+    for(int i=0;i<16;i++) {
+      grid[i][3] = color[p++];
+    }
+  } else if(dir == 4) {
+    for(int i=15;i>3;i--) {
+      for(int j=0;j<4;j++) {
+        grid[rot(i)][j] = grid[rot(i-4)][j];
+      }
+    }
+    for(int i=0;i<4;i++) {
+      for(int j=0;j<4;j++) {
+        grid[rot(i)][j] = color[p++];
+      }
+    }
+  } else if(dir == 5) {
+    for(int i=0;i<12;i++) {
+      for(int j=0;j<4;j++) {
+        grid[rot(i)][j] = grid[rot(i+4)][j];
+      }
+    }
+    for(int i=12;i<16;i++) {
+      for(int j=0;j<4;j++) {
+        grid[rot(i)][j] = color[p++];
+      }
+    }
+  }
+  
+  drawgrid(m);
 }
 
 void mdraw(mode_t *m,int mode) {
@@ -230,19 +419,6 @@ void mdrip(mode_t *m,int mode) {
   grid[on][0] = m->patt[loc];
 
   drawgrid(m);
-/*
-  for(int i=0;i<16;i++) {
-    for(int j=0;j<4;j++) {
-      int off = j * 16;
-      int ii = i;
-      if((j%2)==1) ii = 15-i;
-
-      int row = j;
-      if(m->flags & 1) row = 3 - j;
-      leds[ii+off] = grid[i][row];
-    }  
-  }
-  */
 }
 
 void mrand(mode_t *m,int mode) {
@@ -268,6 +444,7 @@ void mrand(mode_t *m,int mode) {
 
 void mm(mode_t *m,int mode) {
   unsigned int pos = gpos;// % m->num_patt;
+  if(m->flags & 4) pos /= 4;
 
   if(b5.state) {
     ppos++;
@@ -376,30 +553,7 @@ void loop(){
   }
 
   (*modes[mode].mfunc)(modes+mode,mode);
-  /*
-  switch(mode) {
-    case 0: m0(modes+mode,mode); break;
-    default: mm(modes+mode,mode); break;
-  }
-  */
 
-  /*
-  for(int i = 0; i < num_leds; i++) {
-    int loc;
-    if(mode == 0) loc = (pos+i)%num_patt;
-    else loc = pos%num_patt;
-    
-    for(int j=0;j<num_chunks;j++) {
-      int off = j*num_leds;
-      int ii = i;
-      if((j%2)==1) ii = (num_leds-1)-i;
-      
-      CRGB color = patt[loc];
-      if(mode == 2) color = 0xffffff;
-      leds[ii+off] = color; //patt[loc];
-    }
-  }
-*/
   FastLED.show();
   FastLED.delay(1000/fps); 
 }
